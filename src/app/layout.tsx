49de35a407967c0e2a970de6b5182df1ed9c13d3
LucasButto <lbutto40@gmail.com>
2024-09-20 15:52:44 -0300

    feature: implemented componets created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,27 @@ import type { Metadata } from "next";
 import NavBar from "@/components/NavBar/NavBar";
 import "@/styles/globals.scss";
 
+import { Oxygen, Urbanist } from "next/font/google";
+import WhatsAppFloatingIcon from "@/components/WhatsAppFloatingIcon/WhatsAppFloatingIcon";
+import { StatesProvider } from "@/contexts/StatesContext";
+
 export const metadata: Metadata = {
   title: "Arquitecto - Julian Peisino",
   description:
     "Arquitecto. Facultad de Arquitectura, Planeamiento y Diseño. UNR. Rosario año 1995 / CAPSF 02996",
 };
+
+const urbanist_font = Urbanist({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-urbanist",
+});
+
+const oxygen_font = Oxygen({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--font-oxygen",
+});
 
 export default function RootLayout({
   children,
@@ -21,10 +37,12 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/img/favicon/favicon.ico" />
       </head>
-      <body>
-        <NavBar />
-        {children}
-        {/* WhatsAppFloatingIcon */}
+      <body className={urbanist_font.variable + " " + oxygen_font.variable}>
+        <StatesProvider>
+          <NavBar />
+          {children}
+          <WhatsAppFloatingIcon />
+        </StatesProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import HomeSectionMain from "@/components/HomeSections/HomeSectionMain/HomeSectionMain";
+import SideBarButtons from "@/components/SideBarButtons/SideBarButtons";
+import HomeSectionRight from "@/components/HomeSections/HomeSectionRight/HomeSectionRight";
 
 export default function Home() {
   return (
     <main>
+      <SideBarButtons />
       <HomeSectionMain />
-      <HomeSectionMain />
-      <HomeSectionMain />
-      <HomeSectionMain />
+      <HomeSectionRight />
     </main>
   );
 }
