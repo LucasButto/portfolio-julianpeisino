91d2c614b6ca812309b70c98a7891b6b27a507d3
LucasButto <lbutto40@gmail.com>
2024-09-20 15:50:05 -0300

    feature: Whatsapp button component created

---
+++ src/components/WhatsAppFloatingIcon/WhatsAppFloatingIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "next/link";
+import "./WhatsAppFloatingIcon.scss";
+//import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import Image from "next/image";
+
+const WhatsAppFloatingIcon = () => {
+  return (
+    <div className="icon-container">
+      <Link
+        href="https://wa.me/5493462365854"
+        className="icon"
+        target="_blank"
+        passHref
+      >
+        <Image src="/img/Wpp.webp" alt="WhatsApp" width={35} height={35} />
+      </Link>
+    </div>
+  );
+};
+
+export default WhatsAppFloatingIcon;
