f118b4b63345c93a758fccb43c0ecfeab2912ef7
LucasButto <lbutto40@gmail.com>
2024-09-22 22:50:38 -0300

    feature: added de new components to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,19 @@ import HomeSectionMain from "@/components/HomeSections/HomeSectionMain/HomeSecti
 import SideBarButtons from "@/components/SideBarButtons/SideBarButtons";
 import HomeSectionRight from "@/components/HomeSections/HomeSectionRight/HomeSectionRight";
 import HomeSectionLeft from "@/components/HomeSections/HomeSectionLeft/HomeSectionLeft";
+import StudyInfo from "@/components/StudyInfo/StudyInfo";
+import Contact from "@/components/Contact/Contact";
 
 export default function Home() {
   return (
     <main>
       <SideBarButtons />
       <HomeSectionMain />
-      <HomeSectionRight />
-      <HomeSectionLeft />
+      <HomeSectionRight projectId="viviendas" projectTitle="VIVIENDAS" />
+      <HomeSectionLeft projectId="deptosph" projectTitle="DEPTOS PH" />
+      <HomeSectionRight projectId="comercial" projectTitle="COMERCIAL" />
+      <StudyInfo />
+      <Contact />
     </main>
   );
 }
