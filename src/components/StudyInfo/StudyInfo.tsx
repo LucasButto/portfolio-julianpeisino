9da0b264b017fbd8aac2edcb0b9888d677887d94
LucasButto <lbutto40@gmail.com>
2024-09-22 22:46:09 -0300

    feature: added StudyInfo component

---
+++ src/components/StudyInfo/StudyInfo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./StudyInfo.scss";
+
+const StudyInfo = () => {
+  return (
+    <section
+      className="home-section-container-study info-background"
+      id="estudio"
+    >
+      <div className="left-container"></div>
+      <div className="right-container"></div>
+    </section>
+  );
+};
+
+export default StudyInfo;
