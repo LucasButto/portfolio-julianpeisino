ac43c9f35cbe94ac09baad2725ade8582f21724f
LucasButto <lbutto40@gmail.com>
2024-09-28 04:37:02 -0300

    feature: footer first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2021 Julian Peisino</p>
+    </footer>
+  );
+};
+
+export default Footer;
